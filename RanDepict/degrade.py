3f1023de84b084f616b25a316a1f8989addd4dde
Iagea <iagea1993@gmail.com>
2022-06-10 16:54:37 +0200

    Update degrade.py
    flake8

--- RanDepict/degrade.py
+++ RanDepict/degrade.py
@@ -1,38 +1,40 @@
-'''
-This script has been modified from the original degrade.py present on  
-https://github.com/mtzgroup/ChemPixCH 
+"""
+This script has been modified from the original degrade.py present on
+https://github.com/mtzgroup/ChemPixCH
 
 From the publication:
 https://pubs.rsc.org/en/content/articlelanding/2021/SC/D1SC02957F
-'''
+"""
 
 import cv2
 import numpy as np
 from PIL import Image, ImageEnhance
 
+
 def contrast(img):
     """
     This function randomly changes the input image contrast.
-    
+
     Args:
         img: the image to modify in array format.
     Returns:
         img: the image with the contrast changes.
     """
-    if np.random.uniform(0, 1)<0.8: # increase contrast
+    if np.random.uniform(0, 1) < 0.8:  # increase contrast
         f = np.random.uniform(1, 2)
-    else: # decrease contrast
+    else:  # decrease contrast
         f = np.random.uniform(0.5, 1)
     im_pil = Image.fromarray(img)
     enhancer = ImageEnhance.Contrast(im_pil)
-    im  = enhancer.enhance(f)
+    im = enhancer.enhance(f)
     img = np.asarray(im)
     return np.asarray(im)
+
 
 def brightness(img):
     """
     This function randomly changes the input image brightness.
-    
+
     Args:
         img: the image to modify in array format.
     Returns:
@@ -41,33 +43,34 @@ def brightness(img):
     f = np.random.uniform(0.4, 1.1)
     im_pil = Image.fromarray(img)
     enhancer = ImageEnhance.Brightness(im_pil)
-    im  = enhancer.enhance(f)
+    im = enhancer.enhance(f)
     img = np.asarray(im)
     return np.asarray(im)
+
 
 def sharpness(img):
     """
     This function randomly changes the input image sharpness.
-    
+
     Args:
         img: the image to modify in array format.
     Returns:
         img: the image with the sharpness changes.
     """
-    if np.random.uniform(0,1) < 0.5: # increase sharpness
+    if np.random.uniform(0, 1) < 0.5:  # increase sharpness
         f = np.random.uniform(0.1, 1)
-    else: # decrease sharpness
+    else:  # decrease sharpness
         f = np.random.uniform(1, 10)
     im_pil = Image.fromarray(img)
     enhancer = ImageEnhance.Sharpness(im_pil)
-    im  = enhancer.enhance(f)
+    im = enhancer.enhance(f)
     img = np.asarray(im)
     return np.asarray(im)
 
 def s_and_p(img):
     """
     This function randomly adds salt and pepper to the input image.
-    
+
     Args:
         img: the image to modify in array format.
     Returns:
@@ -80,30 +83,29 @@ def s_and_p(img):
     out = np.copy(img)
     # Salt mode
     num_salt = np.ceil(amount * img.size * s_vs_p)
-    coords = [np.random.randint(0, i - 1, int(num_salt))
-          for i in img.shape]
+    coords = [np.random.randint(0, i - 1, int(num_salt)) for i in img.shape]
     out[tuple(coords)] = 1
-    #pepper
-    num_pepper = np.ceil(amount* img.size * (1. - s_vs_p))
-    coords = [np.random.randint(0, i - 1, int(num_pepper))
-          for i in img.shape]
+    # pepper
+    num_pepper = np.ceil(amount * img.size * (1.0 - s_vs_p))
+    coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in img.shape]
     out[tuple(coords)] = 0
     return out
+
 
 def scale(img):
     """
     This function randomly scales the input image.
-    
+
     Args:
         img: the image to modify in array format.
     Returns:
         res: the scaled image.
     """
     f = np.random.uniform(0.5, 1.5)
-    shape_OG = img.shape
-    res = cv2.resize(img, None, fx=f, fy=f, interpolation = cv2.INTER_CUBIC)
-    res = cv2.resize(res, None, fx=1.0/f, fy=1.0/f, interpolation = cv2.INTER_CUBIC)
+    res = cv2.resize(img, None, fx=f, fy=f, interpolation=cv2.INTER_CUBIC)
+    res = cv2.resize(res, None, fx=1.0 / f, fy=1.0 / f, interpolation=cv2.INTER_CUBIC)
     return res
+
 
 def degrade_img(img):
     """
@@ -115,7 +117,7 @@ def degrade_img(img):
     Returns:
         img: the degraded image.
     """
-    # s+p    
+    # s+p
     if np.random.uniform(0, 1) < 0.1:
         img = s_and_p(img)
 
@@ -124,8 +126,8 @@ def degrade_img(img):
         img = scale(img)
 
     # brightness
-    if np.random.uniform(0 ,1) < 0.7:
-        img = brightness(img)        
+    if np.random.uniform(0, 1) < 0.7:
+        img = brightness(img)
 
     # contrast
     if np.random.uniform(0, 1) < 0.7:
@@ -135,6 +137,6 @@ def degrade_img(img):
     if np.random.uniform(0, 1) < 0.5:
         img = sharpness(img)
 
-    #Modify the next line if you want a particular image size as output
-    #img = cv2.resize(img, (256, 256))
+    # Modify the next line if you want a particular image size as output
+    # img = cv2.resize(img, (256, 256))
     return img
