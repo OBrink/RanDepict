b3c004372f4dc1c9ad4e8597332b8c5162496dbf
Otto Brinkhaus <brinkhaus@gmail.com>
2022-09-30 12:59:51 +0200

    Update batch depiction script

--- examples/create_random_depictions.py
+++ examples/create_random_depictions.py
@@ -1,221 +1,187 @@
 import os
-from typing import List, Tuple
+from typing import Tuple, List
 import argparse
+from multiprocessing import Process
+import time
+import random
+from skimage.util import img_as_ubyte
+from skimage import io as sk_io
+
 from RanDepict import RandomDepictor
-from multiprocessing.pool import MaybeEncodingError
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-def parse_arguments() -> argparse.Namespace:
+class RandomDepictor(RandomDepictor):
     """
-    Parse arguments with argparse.
-
-    Returns:
-        Returns an namespace object that holds the given arguments
+    This class is a child of RanDepict.RandomDepictor and contains some
+    additional functions to write the images.
     """
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file", nargs="+")
-    return parser.parse_args()
+    def __init__(
+        self,
+        seed=(random.randint(0, 100))
+    ) -> None:
+        super().__init__(seed=seed)
 
+    def batch_depict_save(
+        self,
+        smiles_list: List[str],
+        n_non_augmented: int,
+        n_augmented: int,
+        ID_list: List[str],
+        chunksize: int,
+        shape: Tuple[int, int] = (299, 299),
+        num_processes: int = 20,
+        seed: int = (random.randint(0, 100)),
+        timeout_limit: int = 1800
+    ) -> None:
+        """
+        This function takes a list of SMILES str, the amount of images
+        to create per SMILES str and the path of an output directory.
+        It then creates images_per_structure depictions of each chemical
+        structure that is represented by the SMILES strings and saves them
+        in in output_dir. If an ID list (list with names of
+        same length as smiles_list that contains unique IDs), the IDs will
+        be used to name the files.
+        """
 
-def read_input_file(filename: str) -> Tuple[List, List]:
-    """
-    Read IDs and SMILES from a file with expected structure:
-    ID1,SMILES1\n
-    ID2,SMILES2\n
-    (...)
+        counter = (n for n in range(int(len(smiles_list)/chunksize)))
 
-    Args:
-        filename (str): Input file name
+        def chunks(lst, n):
+            """Yield successive n-sized chunks from lst."""
+            for i in range(0, len(lst), n):
+                yield lst[i:i + n]
 
-    Returns:
-        Tuple[List, List]: List of IDs, List of SMILES
-    """
-    ids = []
-    smiles = []
-    with open(filename, "r") as fp:
-        for line in fp.readlines():
-            ids.append(line.strip("\n").split(",")[0])
-            smiles.append(line.strip("\n").split(",")[1])
-    return ids, smiles
+        smiles_list = chunks(smiles_list, chunksize)
+        IDs = chunks(ID_list, chunksize)
 
-
-def coordinate_depiction(
-    ids: List[str],
-    smiles: List[str],
-    image_dir: str,
-    im_per_smiles: int,
-    augment: bool,
-    depiction_img_shape: Tuple[int, int],
-) -> None:
-    """
-    Coordination function for batch depiction. If an error occurs,
-    the process is restarted without re-depicting the already depicted
-    structures.
-
-    Args:
-        ids (List[str])
-        smiles (List[str])
-        image_dir (str)
-        im_per_smiles (int)
-        augment (bool)
-        depiction_img_shape (Tuple[int, int])
-    """
-    # Generate depictions without augmentations
-    unprocessed_IDs = ids
-    while len(unprocessed_IDs) != 0:
-        # If the process has been aborted before,
-        # it can be restarted without re-depicting already depicted structures:
-        unprocessed_IDs, unprocessed_SMILES = update_SMILES_list(
-            zip(ids, smiles), image_dir
+        async_proc_args = (
+            (
+                next(smiles_list),
+                n_non_augmented,
+                n_augmented,
+                shape,
+                next(IDs),
+                (seed * n + 1),  # individual seed
+            )
+            for n in counter
         )
-        if len(unprocessed_IDs) != 0:
-            print(
-                "Start depicting {} structures (no augmentations).".format(
-                    len(unprocessed_IDs)
-                )
-            )
-            try:
-                with RandomDepictor() as depictor:
-                    depictor.batch_depict_save(
-                        unprocessed_SMILES,
-                        im_per_smiles,
-                        image_dir,
-                        augment,
-                        unprocessed_IDs,
-                        depiction_img_shape,
-                        10,
-                    )
-            except MaybeEncodingError as e:
-                print(e)
-                print(
-                    "The error is ignored for now and the depiction",
-                    "processes is restarted (already depicted structures",
-                    "are not depicted again)",
-                )
-        else:
-            print(
-                "Done. All structures have been depicted",
-                "Augmentations: {}".format(augment),
-            )
+        process_list = []
+        while True:
+            for proc, init_time in process_list:
+                # Remove finished processes
+                if not proc.is_alive():
+                    process_list.remove((proc, init_time))
+                # Remove timed out processes
+                elif time.time() - init_time >= timeout_limit:
+                    process_list.remove((proc, init_time))
+                    proc.terminate()
+                    proc.join()
+            if len(process_list) < num_processes:
+                # Start new processes
+                for _ in range(num_processes-len(process_list)):
+                    try:
+                        p = Process(target=self.depict_save,
+                                    args=next(async_proc_args))
+                        process_list.append((p, time.time()))
+                        p.start()
+                    except StopIteration:
+                        break
+            if len(process_list) == 0:
+                break
 
 
-def write_paths_to_file(
-    input_filename: str,
-    ids: List[str],
-    smiles: List[str],
-    non_aug_image_path: str,
-    im_per_smiles_non_aug: int,
-    aug_image_path: str,
-    im_per_smiles_aug: int,
-) -> None:
-    """
-    Write file with paths of saved images
-
-    Args:
-        input_filename (str)
-        ids (List[str])
-        smiles (List[str])
-        non_aug_image_path (str)
-        im_per_smiles_non_aug (int)
-        aug_image_path (str)
-        im_per_smiles_aug (int)
-    """
-    with open(input_filename + "_paths", "w") as fp:
-        for i in range(len(ids)):
-            for j in range(im_per_smiles_non_aug):
-                fp.write(
-                    non_aug_image_path + ids[i] + "_" + str(j) + "," + smiles[i] + "\n"
-                )
-            for k in range(im_per_smiles_aug):
-                fp.write(
-                    aug_image_path + ids[i] + "_" + str(k) + "," + smiles[i] + "\n"
-                )
-
-
-def update_SMILES_list(
-    ID_SMILES_tuples: List[Tuple[str, str]],
-    path: str,
-) -> Tuple[List[str], List[str]]:
-    """
-    This function takes a List of tuples containing IDs (str) and SMILES (str)
-    and the output path (str). It checks which of the corresponding structures
-    already have been depicted and returns two lists of IDs and SMILES of
-    undepicted structures.
-    This way, we don't have to start from scratch if the process was aborted.
-    """
-    # Get list of IDs of already depicted structures
-    already_processed = [img_name.split("_")[0] for img_name in os.listdir(path)]
-    # Get list of SMILES of not-yet depicted structures
-    updated_ID_SMILES_tuples = [
-        (tup[0], tup[1]) for tup in ID_SMILES_tuples if tup[0] not in already_processed
-    ]
-    if len(updated_ID_SMILES_tuples) != 0:
-        IDs, SMILES = zip(*updated_ID_SMILES_tuples)
-    else:
-        IDs = []
-        SMILES = []
-    return IDs, SMILES
+    def depict_save(
+        self,
+        smiles: List[str],
+        n_non_augmented: int,
+        n_augmented: int,
+        shape: Tuple[int, int],
+        ID: List[int],
+        seed: int = (random.randint(0, 100)),
+    ):
+        """
+        This function takes a list of SMILES str, the amount of
+        images to create per SMILES str and the path of an output
+        directory. It then creates images_per_structure depictions
+        of the chemical structure that is represented by the SMILES
+        strings and saves it in output_dir.
+        The given ID is used as the base filename.
+        """
+        depictor = RandomDepictor(seed + 13)
+        for smiles_index in range(len(smiles)):
+            smi = smiles[smiles_index]
+            for _ in range(n_augmented):
+                for n in range(5):
+                    try:
+                        output_path = f"RanDepict_dataset/{ID[smiles_index]}_aug_{n}.png"
+                        sk_io.imsave(output_path, img_as_ubyte(depictor(smi, shape)))
+                        break
+                    except Exception as e:
+                        print(f'An exception occured:\n{e}',
+                              'It is ignored for now and we simply try to depict this structure again...')
+                else:
+                    print('FAILED depicting structure: {}. Tried five times.'.format(smi))
+            for _ in range(n_non_augmented):
+                for _ in range(5):
+                    try:
+                        output_path = f"RanDepict_dataset/{ID[smiles_index]}_aug_{n}.png"
+                        sk_io.imsave(output_path, img_as_ubyte(depictor(smi, shape)))
+                        break
+                    except Exception as e:
+                        print(f'An exception occured:\n{e}',
+                              'It is ignored for now and we simply try to depict this structure again...')
+                else:
+                    print('FAILED depicting structure: {}. Tried five times.'.format(smi))
+        return
 
 
 def main() -> None:
-    """
-    This script reads SMILES corresponding IDs from a file and generates three
-    augmented and three non-augmented depictions per chemical structure.
-    The images are saved in $filename_augmented/$filename_not_augmented.
+    '''
+    This script reads a SMILES, the annotationa and IDs from a file and
+    generates three augmented and three non-augmented depictions per chemical structure.
+    The images are saved.
     ___
     Structure of input file:
-    ID1,SMILES1\n
-    ID2,SMILES2\n
+    ID1,SMILES1,annotation\n
+    ID2,SMILES2,annotation\n
     (...)
+    The annotation is an array saved as a str ([1  2  3  4  ..  .. X])
     ___
-    """
+    '''
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", nargs="+")
+    args = parser.parse_args()
 
-    args = parse_arguments()
-    ids, smiles = read_input_file(args.file[0])
+    # Read input data from file
+    ids = []
+    smiles = []
+    with open(args.file[0], "r") as fp:
+        for line in fp.readlines():
+            ids.append(line.strip("\n").split("\t")[0])
+            smiles.append(line.strip("\n").split("\t")[1])
 
-    # Set desired image shape/number of depictions per SMILES and output paths
-    im_per_smiles_non_aug = 3
-    im_per_smiles_aug = 3
+    if not os.path.exists('RanDepict_dataset'):
+        os.mkdir('RanDepict_dataset')
+
+    # Set desired image shape and number of depictions per SMILES and output paths
+    im_per_SMILES_noaug = 1
+    im_per_SMILES_aug = 1
     depiction_img_shape = (299, 299)
-    aug_image_path = os.path.normpath(args.file[0] + "_augmented")
-    not_aug_image_path = os.path.normpath(args.file[0] + "_not_augmented")
 
-    # Create output directories if necessary
-    if not os.path.exists(aug_image_path):
-        os.mkdir(aug_image_path)
-    if not os.path.exists(not_aug_image_path):
-        os.mkdir(not_aug_image_path)
-
-    # Generate  depictions without augmentations
-    coordinate_depiction(
-        ids,
-        smiles,
-        not_aug_image_path,
-        im_per_smiles_non_aug,
-        augment=False,
-        depiction_img_shape=depiction_img_shape,
-    )
-
-    # Generate depictions with augmentations
-    coordinate_depiction(
-        ids,
-        smiles,
-        aug_image_path,
-        im_per_smiles_aug,
-        augment=True,
-        depiction_img_shape=depiction_img_shape,
-    )
-
-    write_paths_to_file(
-        args.file[0],
-        ids,
-        smiles,
-        not_aug_image_path,
-        im_per_smiles_non_aug,
-        aug_image_path,
-        im_per_smiles_aug,
-    )
+    SMILES_chunksize = 1500
+    with RandomDepictor() as depictor:
+        depictor.batch_depict_save(
+            smiles,
+            im_per_SMILES_noaug,
+            im_per_SMILES_aug,
+            ids,
+            SMILES_chunksize,
+            depiction_img_shape,
+            20,
+            random.randint(0, 100),
+            1800)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
